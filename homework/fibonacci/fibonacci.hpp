34ba7d69021f963f14457ee094f4c372a50a9be1
ctrl-daan <danf@poczta.fm>
2025-03-10 12:22:08 +0100

    iterative method added, test passed

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1) {
+        return sequence;
+    }
+
+    int previous1 = 1, previous2 = 0;
+    int current{};
+    for (int i = 2; i <= sequence; i++) {
+        current = previous1 + previous2;
+        previous2 = previous1;
+        previous1 = current;
+    }
+    return current;
 }
 
 int fibonacci_recursive(int sequence) {
