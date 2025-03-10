68bdcae85d6381565f75151877957155a5f0a465
ctrl-daan <danf@poczta.fm>
2025-03-10 10:18:50 +0100

    calculate passed test

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,12 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
+    int result{};
+    if (command == "add")
+    {
+        result = first + second;
+    }
+    
     // TODO: Implement your solution here and return proper value
-    return "";
+    return std::to_string(result);
 }
