cca8f22a2ae02537634d15db9843c552e567ed7a
ctrl-daan <danf@poczta.fm>
2025-03-10 10:36:17 +0100

    substract passed test

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,12 +2,16 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    int result{};
-    if (command == "add")
-    {
-        result = first + second;
+    std::string result;
+
+    if (command == "add") {
+        result = std::to_string(first + second);
+    } else if (command == "substract") {
+        result = std::to_string(first - second);
+    } else {
+        result = "Invalid data";
     }
-    
+
     // TODO: Implement your solution here and return proper value
-    return std::to_string(result);
+    return result;
 }
