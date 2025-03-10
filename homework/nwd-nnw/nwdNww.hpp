f7e28d4ce6d3f7bbaa72d7e069e665557c80b3ac
ctrl-daan <danf@poczta.fm>
2025-03-10 14:08:39 +0100

    NWD function added, test is passing

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,27 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs < 0)
+        lhs = lhs * (-1);
+    else if (rhs < 0)
+        rhs = rhs * (-1);
+
+    //--------------------------------
+    if (lhs == 0) {
+        return rhs;
+    } else if (rhs == 0) {
+        return lhs;
+    }
+    //--------------------------------
+    if (lhs == rhs) {
+        return lhs;
+    }
+    //--------------------------------
+    if (lhs > rhs) {
+        return NWD(lhs - rhs, rhs);
+    } else {
+        return NWD(lhs, rhs - lhs);
+    }
 }
 
 int NWW(int lhs, int rhs) {
