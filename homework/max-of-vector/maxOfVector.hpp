6b958bd4acddbf8020696fa8aaa4b00cebbfdae4
ctrl-daan <danf@poczta.fm>
2025-03-12 15:28:09 +0100

    implemented maxOfVector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,14 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int num = 0;
+
+    for (const auto &c : vec) {
+        num = std::min(num, c);
+    }
+    for (const auto &n : vec) {
+        num = std::max(num, n);
+    }
+
+    return num;
 }
