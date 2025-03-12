6f4e5d7e36c11add27f1ce84e20aee413c0281c6
ctrl-daan <danf@poczta.fm>
2025-03-12 18:34:12 +0100

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -7,13 +7,11 @@ std::vector<int> generateSequence(int count, int step) {
     int temp = step;
     
     if (count > 0)
-    {
         for (std::size_t i = 0; i < count; i++)
         {
             vec.push_back(temp);
             temp += step;
-        }
-    }            
+        }    
 
     return vec;
 }
