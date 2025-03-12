afa7d38000b89a9a015d8fad77a18df5b7fb6a61
ctrl-daan <danf@poczta.fm>
2025-03-12 15:38:16 +0100

    attempt to adjust clang formating

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,14 +3,17 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
+
     int num = 0;
 
-    for (const auto &c : vec) {
+    for (const auto &c : vec)
+    {
         num = std::min(num, c);
     }
-    for (const auto &n : vec) {
+
+    for (const auto &n : vec)
+    {
         num = std::max(num, n);
     }
-
     return num;
 }
