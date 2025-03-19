bc7667f8070753a3714ef9b6d754b834ca26a31e
ctrl-daan <danf@poczta.fm>
2025-03-19 12:28:21 +0100

    sub10 function implemented, all tests are passing, code refractored

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,35 +1,35 @@
 #include "vectorFunctions.hpp"
 #include <iostream>
 
-std::vector<std::shared_ptr<int>> generate (int count)
-{
+std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> myVec;
-
-    for (size_t i = 0; i < count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         std::shared_ptr<int> ptr = std::make_shared<int>(i);
         myVec.push_back(ptr);
     }
-    
+
     return myVec;
 }
-void print(std::vector<std::shared_ptr<int>> ptr_vec)
-{
-    for (auto c : ptr_vec)
-        {std::cout<<"COTO JEST:"<<*c<<"\n";}
+
+void print(std::vector<std::shared_ptr<int>> ptr_vec) {
+    for (auto c : ptr_vec) {
+        std::cout << *c << "\n";
+    }
 }
-void add10(std::vector<std::shared_ptr<int>> ptr_vec)
-{
+
+void add10(std::vector<std::shared_ptr<int>> ptr_vec) {
     for (auto c : ptr_vec)
-        if (c != nullptr)
+        if (c != nullptr) {
             *c += 10;
-
+        }
 }
-void sub10(int*ptr )
-{
 
+void sub10(int* ptr) {
+    if (ptr != nullptr)
+        *ptr -= 10;
 }
-void sub10(std::vector<std::shared_ptr<int>> ptr_vec)
-{
 
+void sub10(std::vector<std::shared_ptr<int>> ptr_vec) {
+    for (auto c : ptr_vec)
+        sub10(c.get());
 }
--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -5,5 +5,6 @@
 std::vector<std::shared_ptr<int>> generate (int count);
 void print(std::vector<std::shared_ptr<int>>);
 void add10(std::vector<std::shared_ptr<int>>);
-void sub10(const int *ptr);
+void sub10(int *ptr);
 void sub10(std::vector<std::shared_ptr<int>>);
+
