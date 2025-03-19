80194a1dbb2823f7cba2ad6d1b7d9ab1e65d5f14
ctrl-daan <danf@poczta.fm>
2025-03-19 09:56:49 +0100

    doPasswordsMatch() function implemented

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,31 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode) {
+    std::string err;
+
+    return err;
+};
+
+bool doPasswordsMatch(std::string s1, std::string s2) {
+
+    if (s1 == s2)
+        return true;
+    else
+        return false;    
+};
+
+ErrorCode checkPasswordRules(std::string) {
+    ErrorCode myError;
+
+    return myError;
+};
+
+ErrorCode checkPassword(std::string, std::string) {
+    ErrorCode myError;
+
+    return myError;
+};
+
+// // TODO: I'm empty :) Put enum and function headers here.
+// /// Don't forget the header guard - #pragma once
