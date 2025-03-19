e51364b3d6958c35e5f3cc617c9ad1b129b4a17d
ctrl-daan <danf@poczta.fm>
2025-03-19 10:15:54 +0100

    getErrorMessage() function implemented

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,18 +1,30 @@
 #include "validation.hpp"
-// TODO: Put implementations here
 
-std::string getErrorMessage(ErrorCode) {
-    std::string err;
+std::string getErrorMessage(const ErrorCode &err) {
+    if (err == ErrorCode::Ok) {
+        return "Ok";
 
-    return err;
+    } else if (err == ErrorCode::PasswordNeedsAtLeastNineCharacters) {
+        return "Error, password needs at least nine characters.";
+
+    } else if (err == ErrorCode::PasswordNeedsAtLeastOneNumber) {
+        return "Error, password needs at least one number";
+    } else if (err == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter) {
+        return "Error, password needs at least one special character";
+    } else if (err == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter) {
+        return "Error, password needs at least one upper case letter";
+    } else if (err == ErrorCode::PasswordsDoNotMatch) {
+        return "Error, passwords do not match.";
+    } else {
+        return "Unknown Error";
+    }
 };
 
-bool doPasswordsMatch(std::string s1, std::string s2) {
-
+bool doPasswordsMatch(const std::string &s1, const std::string &s2) {
     if (s1 == s2)
         return true;
     else
-        return false;    
+        return false;
 };
 
 ErrorCode checkPasswordRules(std::string) {
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -11,12 +11,8 @@ enum class ErrorCode
     PasswordsDoNotMatch,    
 };
 
-std::string getErrorMessage(ErrorCode);
-bool doPasswordsMatch(std::string, std::string);
+
+std::string getErrorMessage(const ErrorCode &err);
+bool doPasswordsMatch(const std::string &s1, const std::string &s2);
 ErrorCode checkPasswordRules(std::string);
 ErrorCode checkPassword(std::string, std::string);
-
-
-
-// TODO: I'm empty :) Put enum and function headers here.
-/// Don't forget the header guard - #pragma once
